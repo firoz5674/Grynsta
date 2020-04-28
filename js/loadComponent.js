add7d06b50c3f2d20715034e2f6f1a91c0fa8fbb
shivank-dev <sharmashivank59@gmail.com>
2020-04-28 16:29:50 +0530

    header and footer bug fixed

--- js/loadComponent.js
+++ js/loadComponent.js
@@ -1,4 +1,4 @@
 if ($("#header-wrapper").length !== 0) {
-    $("#header-wrapper").load("../components/header.html");
+    $("#header-wrapper").load("/components/header.html");
     }
-    $("#footer-wrapper").load("../components/footer.html");
+    $("#footer-wrapper").load("/components/footer.html");
